99af022fbc961482615f9c0bcd0155bb05fd2b2f
Monica Verdi <monica.verdi@gmail.com>
2020-11-23 12:46:57 +0100

    createtip fix

--- routes/tipsRouter.js
+++ routes/tipsRouter.js
@@ -6,10 +6,22 @@ const createTip = require("../services/tip/createTip");
 /** 
  * Create a tip
  * Send tip detail
-*/
+
 tipsRouter.post("/", async (req, res) => {
     const tipData =  req.body;
     const newTip = await createTip(tipData);
   
     res.render("TipDetail", newTip);
   })
+ 
+
+  // POST	/private/createtips
+tipsRouter.post("/createtip", (req, res, next) => {
+    // Destructure the values coming from the POST form
+    const { title, description, content} = req.body;
+    Tips.create({ title, description, content})
+      .then((tip) => {
+        res.redirect("/tipslist");
+      })
+      .catch((err) => console.log(err));
+  }); */
--- views/CreateTip.jsx
+++ views/CreateTip.jsx
@@ -11,38 +11,12 @@ function CreateTip(props) {
       <form id="tip-form" action="/private/createtip" method="POST">
         <label>Title</label>
         <br/>
-        <imput type="text" name="tip-tittle" placeholder="Give your tip a tittle!"/>
+        <input type="text" name="title" placeholder="Give your tip a tittle!"/>
         <br/>
-        <imput type="text" name="tip-description" placeholder="2 lines description" />
+        <input type="text" name="description" placeholder="2 lines description" />
         <br/>
-        <imput type="text" name="tip-content" placeholder="You can write your tip here"/>
-        <button ttype="submit">Create Tip</button>
-
-        {
-          props.errorMessage ? 
-            (<div classnName="error-message">{props.errorMessage}</div>):null
-        }
-      </form>
-
-    </Layout>const { PromiseProvider } = require("mongoose");
-const React = require("react");
-const Layout = require("./Layout");
-
-function CreateTip(props) {
-  return (
-    <Layout title="Create Tip">
-
-      <h1>Create new Tip</h1>
-
-      <form id="tip-form" action="/private/createtip" method="POST">
-        <label>Title</label>
-        <br/>
-        <imput type="text" name="tip-tittle" placeholder="Give your tip a tittle!"/>
-        <br/>
-        <imput type="text" name="tip-description" placeholder="2 lines description" />
-        <br/>
-        <imput type="text" name="tip-content" placeholder="You can write your tip here"/>
-        <button ttype="submit">Create Tip</button>
+        <input type="text" name="content" placeholder="You can write your tip here"/>
+        <button type="submit">Create Tip</button>
 
         {
           props.errorMessage ? 
@@ -51,10 +25,6 @@ function CreateTip(props) {
       </form>
 
     </Layout>
-  );
-}
-
-module.exports = CreateTip;
   );
 }
 
