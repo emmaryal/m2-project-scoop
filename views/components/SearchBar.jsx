55ab9ced0ca526ccf8b432055e12043ab6c88f40
Monica Verdi <monica.verdi@gmail.com>
2020-11-25 12:34:09 +0100

    buttons css

--- views/components/SearchBar.jsx
+++ views/components/SearchBar.jsx
@@ -4,7 +4,7 @@ function SearchBar() {
       <div class="cover occupy-space" >
         <h1>Zero waste lifestile tips</h1>
         <form class="flex-form">
-          <input id="seacrh-bar" class="shadow" type="search" placeholder="Ditch papar towels" />
+          <input id="seacrh-bar" autocomplete ="off" class="shadow" type="search" placeholder="Ditch papar towels" />
           <input id="search-button" class="shadow" type="submit" value="Search" />
         </form>
       </div>
